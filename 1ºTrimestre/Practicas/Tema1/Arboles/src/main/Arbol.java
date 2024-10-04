58e3dac9191d5a9647618310b1d159dc896fcb77
mayrabpi <145108717+mayrabpi@users.noreply.github.com>
2024-10-04 23:43:16 +0200

    metodoDelete

--- 1ºTrimestre/Practicas/Tema1/Arboles/src/main/Arbol.java
+++ 1ºTrimestre/Practicas/Tema1/Arboles/src/main/Arbol.java
@@ -19,7 +19,7 @@ public class Arbol {
 		if(valor < current.value) {
 			current.left = addRecursive(current.left, valor);
 		}else if(valor>current.value) {
-			current.rigth = addRecursive(current.rigth, valor);
+			current.right = addRecursive(current.right, valor);
 		}
 		return current;
 	}
@@ -31,12 +31,63 @@ public class Arbol {
 		if(valor == current.value) {
 			return true;// se para
 		}
-		return valor< current.value ? buscarRecursivo(current.left, valor):buscarRecursivo(current.rigth,valor);
+		return valor< current.value ? buscarRecursivo(current.left, valor):buscarRecursivo(current.right,valor);
 	}
 	
 	public boolean buscar(int value) {
 		return buscarRecursivo(this.root, value);
 	}
+	// ---------------------- Methods to delete elements
+	   public void delete(int value) {
+	    root = deleteRecursive(root, value);
+	   }
+	   
+	   private Nodo deleteRecursive(Nodo current, int value) {
+	// If the current node is null, just in case
+	    if (current == null) {
+	    return null;
+	    }
+
+	// In this if sentence, the value of the node to delete was found
+	    if (value == current.value) {
+
+	       
+	       // Case 1: The node is a leaf node
+	       if (current.left == null && current.right == null) {
+	        return null;
+	       }
+	       
+	       // Case 2: The node has one child
+	       if (current.right == null) {
+	        return current.left;
+	       }
+
+	       if (current.left == null) {
+	        return current.right;
+	       }
+	       
+	       // Case 3: The node has two children
+	       int smallestValue = findSmallestValue(current.right);
+	       System.out.println(smallestValue);
+	       current.value = smallestValue;
+	       current.right = deleteRecursive(current.right, smallestValue);
+	    }
+	   
+	// In this if sentence, the value is less than the value of the current node
+	    if (value < current.value) {
+	    current.left = deleteRecursive(current.left, value);
+	    return current;
+	    }
+	   
+	// In this if sentence, the value is bigger than the value of the current node
+	    current.right = deleteRecursive(current.right, value);
+	    return current;
+	   }
+	   
+	   // We’ll use the smallest node of the soon to be deleted node’s right sub-tree
+	   private int findSmallestValue(Nodo root) {
+	    return root.left == null ? root.value : findSmallestValue(root.left);
+	   }
 
 
 }
--- 1ºTrimestre/Practicas/Tema1/Arboles/src/main/Nodo.java
+++ 1ºTrimestre/Practicas/Tema1/Arboles/src/main/Nodo.java
@@ -3,12 +3,12 @@ package main;
 public class Nodo {
 	 int value;
 	 Nodo left;
-	 Nodo rigth;
+	 Nodo right;
 	
 	public  Nodo(int value) {
 		this.value = value;
 		this.left=null;
-		this.rigth=null;
+		this.right=null;
 	}
 
 }
